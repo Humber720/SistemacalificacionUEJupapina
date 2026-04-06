b9316218c571815ba5d941fdb5ca5b8491fdc58a
profesorhumberto <yupconhum321@gmail.com>
2026-04-06 17:50:22 -0400

    lateral.js

--- lateral.js
+++ lateral.js
@@ -77,12 +77,6 @@ function toggleMenu() {
     }
 }
 
-// 🔒 PROTEGER ACCESO
-const estudiante = localStorage.getItem("estudiante");
-
-if (!estudiante) {
-    window.location.href = "index.html";
-}
 // Función para cerrar sesión location.replace()	El historial no permite volver a plataforma
 function logout() {
     // Elimina la sesión
