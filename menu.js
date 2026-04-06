31acad400330b0bfab55080cef1538dd1efb9d0b
profesorhumberto <yupconhum321@gmail.com>
2026-04-06 17:51:23 -0400

    menu.js

--- menu.js
+++ menu.js
@@ -227,12 +227,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// 🔒 PROTEGER ACCESO
-const estudiante = localStorage.getItem("estudiante");
-
-if (!estudiante) {
-    window.location.href = "index.html";
-}
 // Función para cerrar sesión location.replace()	El historial no permite volver a plataforma
 function logout() {
     // Elimina la sesión
